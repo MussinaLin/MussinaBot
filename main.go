d64741be783b14b30ad686f76f8bf0c525d71137
Mussina <mussinalin@gmail.com>
2020-06-01 22:55:04 +0800

    feat: add AES

--- main.go
+++ main.go
@@ -1,25 +1,26 @@
 package main
 
-
 import (
-	"fmt"
-	//"github.com/bitfinexcom/bitfinex-api-go/v2"
-	"github.com/bitfinexcom/bitfinex-api-go/v2/rest"
-	//"os"
+	"log"
+	"os"
 )
 
 func main() {
-	//key := os.Getenv("BFX_KEY")
-	//secret := os.Getenv("BFX_SECRET")
-	key := ""
-	secret := ""
-	c := rest.NewClientWithURL("https://api.bitfinex.com/v2/").Credentials(key, secret)
+	key := os.Getenv("key")
+	log.Println("get key:", key)
+	log.Println("Starting the application...")
+	//plaintext := encryption.Decrypt(key, ciphertext)
+	//fmt.Printf("Decrypted: %s\n", plaintext)
 
-	snapHist, err := c.Funding.OfferHistory("fUSD")
-	if err != nil {
-		panic(err)
-	}
-	for _, item := range snapHist.Snapshot {
-		fmt.Println(item)
-	}
+	//key := "BmkGCnViEQ_FsFlqdk6li74oDVH41fag7dwdVQt8CIL9FlDst2ebmh_Zm3eXRSN-YxPpf6K4uOLZm74="
+	//secret := "Zhz0MxMkxPQ0VfBai-Wzi6nxPGaCw3bYYxizHzgpFnMpY5Hoa2F0WGWQv66nl6ayrHpSUbO9nIgeISw="
+	//c := rest.NewClientWithURL("https://api.bitfinex.com/v2/").Credentials(key, secret)
+	//
+	//snapHist, err := c.Funding.OfferHistory("fUSD")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for _, item := range snapHist.Snapshot {
+	//	fmt.Println(item)
+	//}
 }
