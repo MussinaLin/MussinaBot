78600624a29cca856b6218b9480c5fead82486b5
Mussina <mussinalin@gmail.com>
2020-06-03 23:02:35 +0800

    feat: Ticker.(Schedule job)

--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"MussinaBot/utils"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
@@ -12,6 +16,13 @@ func main() {
 	}
 
 	cfg=cfg
+	// periodic job
+	tick := time.NewTicker(time.Second * 2)
+	go scheduler(tick)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+	tick.Stop()
 	//c := rest.NewClientWithURL("https://api.bitfinex.com/v2/").Credentials(key, secret)
 	//
 	//snapHist, err := c.Funding.OfferHistory("fUSD")
@@ -21,4 +32,9 @@ func main() {
 	//for _, item := range snapHist.Snapshot {
 	//	fmt.Println(item)
 	//}
+}
+func scheduler(tick *time.Ticker) {
+	for t := range tick.C {
+		log.Println(t)
+	}
 }
