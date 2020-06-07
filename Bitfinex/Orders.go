2f023a2fc3a6adc3c922abea97f6d5b4751e3aa8
Mussina <mussinalin@gmail.com>
2020-06-07 21:52:20 +0800

    feat: order struct add period

--- Bitfinex/Orders.go
+++ Bitfinex/Orders.go
@@ -1,36 +1,27 @@
 package Bitfinex
 
-import "math"
-
 type MussinaOrder struct{
 	Amount float64
 	Rate float64
+	Period int32
 }
 
 
 
-func GenOrders(availableBalance float64, maxSignleOrderAmount float64, left float64) int{
+func GenOrders(availableBalance float64, maxSignleOrderAmount float64, minLoan float64, left float64) int{
 	availableBalance -= left
 	orders := make([]MussinaOrder, 0)
 	num := 0
-	for availableBalance > 0{
+	for availableBalance > 0 && availableBalance >= minLoan{
 		num++
 		if availableBalance < maxSignleOrderAmount{ // last order
 			availableBalance = 0
-			orders= append(orders, MussinaOrder{Amount:availableBalance, Rate:0})
+			orders= append(orders, MussinaOrder{Amount:availableBalance, Rate:0, Period:2})
 		}else{
 			availableBalance -= maxSignleOrderAmount
-			orders= append(orders, MussinaOrder{Amount:maxSignleOrderAmount, Rate:0})
+			orders= append(orders, MussinaOrder{Amount:maxSignleOrderAmount, Rate:0, Period:2})
 		}
 	}
 	return num
 }
 
-func calNumOfOrders(availableBalance float64, maxSignleOrderAmount float64) int64{
-	num := int64(availableBalance / maxSignleOrderAmount)
-
-	if math.Mod(availableBalance, maxSignleOrderAmount) != 0{
-		num += 1
-	}
-	return num
-}
--- Bitfinex/Orders_test.go
+++ Bitfinex/Orders_test.go
@@ -4,46 +4,11 @@ import (
 	"testing"
 )
 
-
-func Test_calNumOfOrders(t *testing.T) {
-	type args struct {
-		availableBalance     float64
-		maxSignleOrderAmount float64
-	}
-	tests := []struct {
-		name string
-		args args
-		want int64
-	}{
-		{
-			name :"test1" ,
-			args:args{availableBalance:51, maxSignleOrderAmount:1000},
-			want:1,
-		},
-		{
-			name :"test2",
-			args:args{availableBalance:2000, maxSignleOrderAmount:1000},
-			want:2,
-		},
-		{
-			name :"test3",
-			args:args{availableBalance:1050, maxSignleOrderAmount:1000},
-			want:2,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := calNumOfOrders(tt.args.availableBalance, tt.args.maxSignleOrderAmount); got != tt.want {
-				t.Errorf("calNumOfOrders() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestGenOrders(t *testing.T) {
 	type args struct {
 		availableBalance     float64
 		maxSignleOrderAmount float64
+		minLoan				 float64
 		left                 float64
 	}
 	tests := []struct {
@@ -53,18 +18,23 @@ func TestGenOrders(t *testing.T) {
 	}{
 		{
 			name:"test1",
-			args:args{availableBalance:50,maxSignleOrderAmount:1000,left:0},
+			args:args{availableBalance:50,maxSignleOrderAmount:1000,minLoan:50, left:0},
 			want:1,
 		},
 		{
 			name:"test2",
-			args:args{availableBalance:2000,maxSignleOrderAmount:1000,left:0},
+			args:args{availableBalance:2000,maxSignleOrderAmount:1000,minLoan:50,left:0},
+			want:2,
+		},
+		{
+			name:"test3",
+			args:args{availableBalance:2030,maxSignleOrderAmount:1000,minLoan:50,left:0},
 			want:2,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := GenOrders(tt.args.availableBalance, tt.args.maxSignleOrderAmount, tt.args.left); got != tt.want {
+			if got := GenOrders(tt.args.availableBalance, tt.args.maxSignleOrderAmount,tt.args.minLoan, tt.args.left); got != tt.want {
 				t.Errorf("GenOrders() = %v, want %v", got, tt.want)
 			}
 		})
